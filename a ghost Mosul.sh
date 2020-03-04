fc82115d6b78e14cd654dd01535fd18b119e30f5
zeroabdullah <47537346+zeroabdullah@users.noreply.github.com>
2020-03-04 09:21:32 +0300

    Update a ghost Mosul.sh

--- a ghost Mosul.sh
+++ a ghost Mosul.sh
@@ -81,7 +81,7 @@ curl -LO https://github.com/termux/termux-packages/files/3995119/metasploit_5.0.
 
 gunzip metasploit_5.0.65-1_all.deb.gz
 
-dpkg -i metasploit_5.0.65-1_all.deb
+dpkg -i metasploit_5.0.65-1_all.deb -y
 
 apt -f install
 
